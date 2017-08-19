287ec265852ca6298a91be3bbb64866cbd178ac8
Hugo Hernani <hugo@quezmedia.com>
2017-08-18 23:30:59 -0300

    Basic Routing For Doorkeeper and API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications,
+    :authorized_applications
+  end
+  mount API::Base => '/api' # Grape
 end
