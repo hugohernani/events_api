9c7dbbcfaf511a7220102413a23eb710efc982f8
Hugo Hernani <hugo@quezmedia.com>
2017-08-18 23:33:21 -0300

    User Model Added With Basic Authenticate Method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ActiveRecord::Base
          :recoverable,
          :rememberable,
          :trackable,
-         :validatable,
-         :omniauthable
-  include DeviseTokenAuth::Concerns::User
+         :validatable
+
+  class << self
+    def authenticate(email, password)
+      user = User.find_for_authentication(email: email)
+      user.try(:valid_password?, password) ? user : nil
+    end
+  end
 end
